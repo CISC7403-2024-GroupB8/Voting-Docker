d4c2b60471328b452f44476936750a798b7e24b9
Rookie, LIN <1049825685@qq.com>
2024-11-30 21:54:44 +0800

    :art: 整理結合前端兩個網頁的yml

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ConfettiExplosion from 'react-confetti-explosion';
 
-const ENV_PORT = process.env.REACT_APP_PORT;
+// set PORT=5000 && npm start 修改端口，6110投票 6111結果 
+const ENV_PORT = window.location.port;
 console.log("The application is running on port:", ENV_PORT);
+
+// localhost本地開發，backend_app容器開發，似乎不需要，只要空出localhost的8000端口即可
+// const HOST = 'backend_app';
+const HOST = 'localhost';
 
 function App() {
   const [catCount, setCatCount] = useState(0);
@@ -12,99 +17,151 @@ function App() {
   const [voteOption, setVote] = useState(null);
   const [isVoted, setIsVoted] = useState(false);
   const [showExplosion, setShowExplosion] = useState(false);
+  const [lastVote, setLastVote] = useState(null);
+  const [catLastVote, setCatLastVote] = useState(null);
+  const [dogLastVote, setDogLastVote] = useState(null);
 
   // Vote 模式
   let totalCount = catCount + dogCount;
   let catPercentage = 50;
   let dogPercentage = 50;
 
-  if (ENV_PORT == '6111') {
+  const webMode = ENV_PORT == '6111' ? 'result' : 'vote';
+  if (webMode == 'result') {
     // TODO: Result 模式
     catPercentage = totalCount === 0 ? 50 : (catCount / totalCount) * 100;
     dogPercentage = totalCount === 0 ? 50 : (dogCount / totalCount) * 100;
   } else {
     // Vote 模式
     if (voteOption) {
-      catPercentage = voteOption === 'Cat' ? 100 : 0;
-      dogPercentage = voteOption === 'Dog' ? 100 : 0;
+      catPercentage = voteOption === 'Cats' ? 100 : 0;
+      dogPercentage = voteOption === 'Dogs' ? 100 : 0;
+    }
+  }
+
+  useEffect(() => {
+    if (webMode == 'result') {
+      get_result();
+    }
+  }, [])
+
+  // 訪問後端投票API
+  const vote = async () => {
+    setIsVoted(true);
+    setShowExplosion(true);
+
+    const res = await fetch(`http://${HOST}:8000/vote`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({ option: voteOption })
+    });
+    console.log('res:', res);
+    if (res.status === 200) {
+      console.log('投票成功');
+      setTimeout(() => setShowExplosion(false), 3000); // 設置動畫持續時間
+    } else {
+      alert('投票失敗');
     }
   }
 
 
-  // TODO: 整合API
-  const vote = async () => {
-    setIsVoted(true);
-    setShowExplosion(true);
-    setTimeout(() => setShowExplosion(false), 3000); // 設置動畫持續時間
-  }
-
-
-  // TODO: 獲取投票結果
+  // 獲取投票結果
   const get_result = async () => {
-
+    const res = await fetch(`http://${HOST}:8000/results`);
+    const data = await res.json();
+    let catJSON = data.find((item) => item.option === 'Cats');
+    let dogJSON = data.find((item) => item.option === 'Dogs');
+    console.log('catJSON:', catJSON);
+    console.log('dogJSON:', dogJSON);
+    setCatCount(catJSON.vote_number);
+    setDogCount(dogJSON.vote_number);
+    setCatLastVote(catJSON.last_vote);
+    setDogLastVote(dogJSON.last_vote);
   }
 
   return (
     <div className="flex flex-row h-screen w-screen">
       <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 '>
-        {!isVoted && (
-          <h1 className="mb-5 text-3xl font-bold text-white hover:scale-110 transition-all">Cats or Dogs?</h1>
-        )}
+        {/* 投票頁 */}
+        {webMode == 'vote' ? (<>
+          {!isVoted && (
+            <h1 className="mb-5 text-3xl font-bold text-white hover:scale-110 transition-all">Cats or Dogs?</h1>
+          )}
 
-        {!isVoted && (<>
+          {!isVoted && (<>
+            <button
+              onClick={() => {
+                // setCatCount(catCount + 1)
+                setVote('Cats')
+              }}
+              className="bg-white  text-green-500 font-bold py-4 px-6 rounded m-2 transition-transform transform 
+            hover:scale-110 hover:text-lg shadow-md"
+              onMouseEnter={() => setIsCatHovered(true)}
+              onMouseLeave={() => setIsCatHovered(false)}
+            >
+              Cats🐱
+              {/* ({catPercentage.toFixed(2)}%) */}
+            </button>
+            <button
+              onClick={() => {
+                // setDogCount(dogCount + 1)
+                setVote('Dogs')
+              }}
+              className="bg-white  text-blue-500 font-bold py-4 px-6 rounded m-2 transition-transform transform 
+            hover:scale-110 hover:text-lg shadow-md"
+              onMouseEnter={() => setIsDogHovered(true)}
+              onMouseLeave={() => setIsDogHovered(false)}
+            >
+              Dogs🐶
+              {/* ({dogPercentage.toFixed(2)}%) */}
+            </button>
+          </>)}
+
+          {voteOption && (<div className='mt-5'>
+            <h1 className="text-3xl font-bold text-white hover:scale-110 transition-all">You Choose {voteOption} ! 🎉🎊</h1>
+
+            {!isVoted && (<button
+              onClick={() => vote()}
+              className="bg-white  text-black font-bold py-4 px-6 rounded m-2 transition-transform transform hover:scale-110 shadow-md"
+            >
+              Confirm Vote
+            </button>)}
+          </div>)}
+
+          {isVoted && (<>
+            {showExplosion && <ConfettiExplosion />}
+            <button
+              onClick={() => {
+                // 查看投票結果，跳轉6111
+                window.location.href = `http://localhost:6111`;
+              }}
+              className="bg-white  text-black font-bold py-4 px-6 rounded m-2 transition-transform transform hover:scale-110 shadow-md"
+            >
+              Check Result 🎉🎉🎉🎊🎊🎊
+            </button>
+          </>)}
+        </>) : (<>
+          {/* 結果頁 */}
+          <h1 className="text-3xl font-bold text-white hover:scale-110 transition-all">Voting Results</h1>
+          <h2 className="text-2xl font-bold text-white hover:scale-110 transition-all">Cats🐱: {catCount} Votes  {catPercentage.toFixed(2)}%</h2>
+          <h1 className="text-l font-bold text-white hover:scale-110 transition-all">Last Vote: {new Date(catLastVote).toLocaleString('zh-CN', { timeZone: 'Asia/Shanghai' })}</h1>
+          <br />
+          <h2 className="text-2xl font-bold text-white hover:scale-110 transition-all">Dogs🐶: {dogCount} Votes  {dogPercentage.toFixed(2)}%</h2>
+          <h1 className="text-l font-bold text-white hover:scale-110 transition-all">Last Vote: {new Date(dogLastVote).toLocaleString('zh-CN', { timeZone: 'Asia/Shanghai' })}</h1>
           <button
             onClick={() => {
-              // setCatCount(catCount + 1)
-              setVote('Cat')
+              // 繼續投票，跳轉6110
+              window.location.href = `http://localhost:6110`;
             }}
-            className="bg-white  text-green-500 font-bold py-4 px-6 rounded m-2 transition-transform transform hover:scale-110 shadow-md"
-            onMouseEnter={() => setIsCatHovered(true)}
-            onMouseLeave={() => setIsCatHovered(false)}
-          >
-            Cat
-            {/* ({catPercentage.toFixed(2)}%) */}
-          </button>
-          <button
-            onClick={() => {
-              // setDogCount(dogCount + 1)
-              setVote('Dog')
-            }}
-            className="bg-white  text-blue-500 font-bold py-4 px-6 rounded m-2 transition-transform transform hover:scale-110 shadow-md"
-            onMouseEnter={() => setIsDogHovered(true)}
-            onMouseLeave={() => setIsDogHovered(false)}
-          >
-            Dog
-            {/* ({dogPercentage.toFixed(2)}%) */}
-          </button>
-        </>)}
-
-        {voteOption && (<div className='mt-5'>
-          <h1 className="text-3xl font-bold text-white hover:scale-110 transition-all">You Choose {voteOption} ! 🎉🎊</h1>
-
-          {!isVoted && (<button
-            onClick={() => vote()}
             className="bg-white  text-black font-bold py-4 px-6 rounded m-2 transition-transform transform hover:scale-110 shadow-md"
           >
-            Confirm Vote
-          </button>)}
-        </div>)}
-
-        {isVoted && (<>
-          {showExplosion && <ConfettiExplosion />}
-          <button
-            onClick={() => {
-              // TODO: 查看投票結果，跳轉6111
-              window.location.href = 'http://localhost:6111';
-            }}
-            className="bg-white  text-black font-bold py-4 px-6 rounded m-2 transition-transform transform hover:scale-110 shadow-md"
-          >
-            Check Result 🎉🎉🎉🎊🎊🎊
+            Vote Again🐱🐶
           </button>
-        </>)}
-      </div>
+        </>)}</div>
 
 
-      {/* ###背景### */}
       {/* 綠色背景 */}
       <div
         // style={{ flexBasis: isCatHovered ? (catPercentage > 80 ? `${catPercentage}%` : '80%') : `${catPercentage}%` }}
